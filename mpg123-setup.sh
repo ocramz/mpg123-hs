1ccf9b26a493020516b4900b0e76cb757aa4750e
Marco Zocca <marco.zocca@recordunion.com>
2017-12-10 15:52:29 +0100

    add mpg123 install script

---
+++ mpg123-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# https://www.mpg123.de/download/mpg123-1.25.8.tar.bz2
+
+VERSION=1.25.8
+ARCHIVE=mpg123-${VERSION}.tar.bz2
+
+printf "\n=== APT-Installing dependencies :\n"
+apt-get update && apt-get install -y --no-install-recommends build-essential bzip2
+
+# # # check env
+printf "\n=== Environment :\n"
+printenv
+
+
+curl -L https://www.mpg123.de/download/${ARCHIVE}
+bunzip2 ${ARCHIVE}
+
+./configure
+make
+make install
