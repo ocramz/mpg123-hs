cc4c27a751361b3e7f6a8dafa6c888b54376dae1
Marco Zocca <marco.zocca@recordunion.com>
2017-12-10 21:20:33 +0100

    debug info

--- mpg123-setup.sh
+++ mpg123-setup.sh
@@ -14,20 +14,20 @@ printf "\n=== making archive directory\n"
 mkdir ${ARCHIVE_DIR}
 
 printf "\n=== downloading mpg123\n"
+# mkdir temp
+# cd temp
+# tar -vxzf /mnt/mydownloads/archive.tar.gz
 curl -L https://www.mpg123.de/download/${ARCHIVE} > ${ARCHIVE}
-bunzip2 ${ARCHIVE} # mpg123-${VERSION}
+tar -vxzf ${ARCHIVE}
+# bunzip2 ${ARCHIVE} # mpg123-${VERSION}
 # wget -c https://www.mpg123.de/download/${ARCHIVE} -O - | bunzip2 > ${ARCHIVE_DIR}
 
-pwd
-
-ls -lsA
+pwd; ls -lsA
 
 printf "\n=== entering build dir\n"
 cd ${ARCHIVE_DIR}
 
-pwd
-
-ls -lsA
+pwd; ls -lsA
 
 printf "\n=== configuring mpg123\n"
 ./configure
