fb58cbe249145b9947b99b89505fd33ae1748b36
Marco Zocca <marco.zocca@recordunion.com>
2017-12-10 16:20:15 +0100

    fix dl

--- mpg123-setup.sh
+++ mpg123-setup.sh
@@ -3,7 +3,8 @@
 # https://www.mpg123.de/download/mpg123-1.25.8.tar.bz2
 
 VERSION=1.25.8
-ARCHIVE=mpg123-${VERSION}.tar.bz2
+ARCHIVE_DIR=mpg123-${VERSION}
+ARCHIVE=${ARCHIVE_DIR}.tar.bz2
 
 printf "\n=== APT-Installing dependencies :\n"
 apt-get update && apt-get install -y --no-install-recommends build-essential bzip2
@@ -12,7 +13,8 @@ apt-get update && apt-get install -y --no-install-recommends build-essential bzi
 
 
 # curl -L https://www.mpg123.de/download/${ARCHIVE} | bunzip2 > mpg123-${VERSION}
-wget -c https://www.mpg123.de/download/${ARCHIVE} -O - | bunzip2
+wget -c https://www.mpg123.de/download/${ARCHIVE} -O - | bunzip2 > ${ARCHIVE_DIR}
+cd ${ARCHIVE_DIR}
 
 ./configure
 make
