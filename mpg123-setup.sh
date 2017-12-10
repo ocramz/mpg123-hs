a78888b5ca4bd6f98956d0100c0ca6edf64274fb
Marco Zocca <marco.zocca@recordunion.com>
2017-12-10 16:31:47 +0100

    add printf

--- mpg123-setup.sh
+++ mpg123-setup.sh
@@ -10,14 +10,20 @@ printf "\n=== APT-Installing dependencies :\n"
 apt-get update && apt-get install -y --no-install-recommends build-essential bzip2
 
 
-
+printf "\n=== making archive directory\n"
 mkdir ${ARCHIVE_DIR}
+
+printf "\n=== downloading mpg123\n"
 curl -L https://www.mpg123.de/download/${ARCHIVE} | bunzip2 > mpg123-${VERSION}
 # wget -c https://www.mpg123.de/download/${ARCHIVE} -O - | bunzip2 > ${ARCHIVE_DIR}
+
 cd ${ARCHIVE_DIR}
 
+printf "\n=== configuring mpg123\n"
 ./configure
+printf "\n=== building mpg123\n"
 make
+printf "\n=== installing mpg123\n"
 make install
 
 
