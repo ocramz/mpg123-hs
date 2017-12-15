14af517731ebc8ccd068e80feffefdb74990058c
Marco Zocca <marco.zocca@recordunion.com>
2017-12-15 20:59:57 +0100

    add cmd line options

--- app/Main.hs
+++ app/Main.hs
@@ -11,7 +11,7 @@ import Data.Semigroup ((<>))
 -- C.include "<math.h>"
 
 
-main = putStrLn "hello!"
+-- main = putStrLn "hello!"
 
 
 -- main :: IO ()
@@ -21,5 +21,68 @@ main = putStrLn "hello!"
 
 
 
+main :: IO ()
+main = greet =<< execParser opts
+  where
+    opts = info (options <**> helper)
+      ( fullDesc
+     -- <> progDesc "Print a greeting for TARGET"
+     -- <> header "hello - a test for optparse-applicative"
+      )
 
-data Options 
+-- greet :: Sample -> IO ()
+-- greet (Sample h False n) = putStrLn $ "Hello, " ++ h ++ replicate n '!'
+-- greet _ = return ()
+
+greet (Options bi bo fi fo ) = putStrLn $ unwords [show bi, show bo, show fi, show fo]
+
+
+bufSizeInDefault = 2^14
+bufSizeOutDefault = 2^15
+
+data Options = Options {
+    bufSizeIn :: Int
+  , bufSizeOut :: Int
+  , fileIn :: FilePath
+  , fileOut :: FilePath
+                        } deriving (Eq, Show)
+
+options :: Parser Options
+options = Options
+  <$> option auto (
+       long "bufSizeIn"
+    <> help "Input buffer size [bytes]"
+    <> showDefault
+    <> value bufSizeInDefault )
+  <*> option auto (
+       long "bufSizeOut"
+    <> help "Output buffer size [bytes]"
+    <> showDefault
+    <> value bufSizeOutDefault )
+  <*> strOption (
+       long "fileIn"
+    <> short 'i'
+    <> help "Input file path")
+  <*> strOption (
+       long "fileOut"
+    <> short 'o'
+    <> help "Output file path")
+
+
+
+-- sample :: Parser Sample
+-- sample = Sample
+--       <$> strOption
+--           ( long "hello"
+--          <> metavar "TARGET"
+--          <> help "Target for the greeting" )
+--       <*> switch
+--           ( long "quiet"
+--          <> short 'q'
+--          <> help "Whether to be quiet" )
+--       <*> option auto
+--           ( long "enthusiasm"
+--          <> help "How enthusiastically to greet"
+--          <> showDefault
+--          <> value 1
+--          <> metavar "INT" )
