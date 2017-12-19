5dc46438c0fd41f6a4191b46949aa0af10f99bcd
ocramz <zocca.marco@gmail.com>
2017-12-19 17:16:47 +0100

    removed conduit and resourcet from here

--- src/Data/Conduit/Audio/Mpg123.hs
+++ src/Data/Conduit/Audio/Mpg123.hs
@@ -2,8 +2,8 @@ module Data.Conduit.Audio.Mpg123 where
 
 import Foreign.Ptr
 
-import qualified Data.Conduit as C
-import Control.Monad.Trans.Resource
+-- import qualified Data.Conduit as C
+-- import Control.Monad.Trans.Resource
 
 import Codec.Mpg123.Internal 
 
